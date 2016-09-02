8c0457af9464e19262d4b99491834695000c2b09
viky <zwj_fhys@qq.com>
2016-09-02 19:27:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+### Status
+[![Build Status](https://travis-ci.org/Viky-zhang/travis-test-java.svg?branch=master)](https://travis-ci.org/Viky-zhang/travis-test-java)
+
+# travis-test-java
+A simple Java Maven project to test travis-ci.
---
+++ src/main/java/HelloMain.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author vikydzhang
+ * @date 2016/9/2
+ */
+public class HelloMain {
+    public static void main(String[] args) {
+        DateTime dt = new DateTime();
+        System.out.println("Viky Time is: " + dt);
+    }
+}
